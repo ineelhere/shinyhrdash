669e4b7c539facfc127eb97a4fa563eac8d42d54
ineelhere <>
2023-05-17 11:46:30 +0530

    [ADD] work in progress

--- app/main.R
+++ app/main.R
@@ -1,5 +1,6 @@
 box::use(
   shiny[...],
+  shinydashboard[...],
 )
 
 box::use(
@@ -10,12 +11,6 @@ box::use(
 ui <- function(id) {
   ns <- NS(id)
   fluidPage(
-  tags$head(
-    tags$meta(charset = "utf-8"),
-    tags$meta(name = "viewport", content = "width=device-width"),
-    tags$link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css", integrity = "sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ", crossorigin = "anonymous")
-  ),
-  tags$body(
     fluidRow(
       column(width = 2,
           left_sidebar$ui(ns("left_sidebar"))),
@@ -24,7 +19,6 @@ ui <- function(id) {
       column(width = 3,
           h1("3rd"))
     )
-  )
   )
 }
 
