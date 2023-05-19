4a7ec03dc2c5bd002e3d26022db857c817eec9cd
ineelhere <>
2023-05-19 11:58:49 +0530

    [ADD] code indentatioins

--- app/main.R
+++ app/main.R
@@ -40,37 +40,37 @@ ui <- function(id) {
           midpage$ui(ns("midpage")),
           br(),
           cards( class="two",
-          card(class = "stats-card",
-            div(class="card-content",
-            h3("Statistics of Active Applications"),
-            echarts4rOutput(ns("stackchart"))
-            )
-        ),
-        card(
-          div(class="card-content",
-            h3("Acquisitions"),
-            plotlyOutput(ns("acquisitions"))
-            )
-        ))
+                 card(size = "wide", class = "stats-card",
+                      div(class="card-content",
+                          h3("Statistics of Active Applications"),
+                          echarts4rOutput(ns("stackchart"))
+                      )
+                 ),
+                 card(
+                   div(class="card-content",
+                       h3("Acquisitions"),
+                       plotlyOutput(ns("acquisitions"))
+                   )
+                 ))
         ),
         div(
           right_sidebar$ui(ns("right_sidebar"))
         )
-        )
       )
     )
+  )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
     data <- data.frame(
-        day = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"),
-        total = c(120, 140, 150, 130, 110, 100, 90),
-        shortlisted = c(80, 90, 70, 80, 60, 50, 40),
-        rejected = c(20, 25, 30, 25, 20, 15, 10),
-        hold = c(20, 25, 50, 25, 30, 35, 40)
-      )
+      day = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"),
+      total = c(120, 140, 150, 130, 110, 100, 90),
+      shortlisted = c(80, 90, 70, 80, 60, 50, 40),
+      rejected = c(20, 25, 30, 25, 20, 15, 10),
+      hold = c(20, 25, 50, 25, 30, 35, 40)
+    )
 
     output$stackchart <- renderEcharts4r({
       stackchart(data)
