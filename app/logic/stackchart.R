1188a0283f2b0cba33bbf02dade462900dc28772
ineelhere <>
2023-05-19 10:50:30 +0530

    [ADD] make hoverable

--- app/logic/stackchart.R
+++ app/logic/stackchart.R
@@ -3,13 +3,17 @@ box::use(
 )
 
 stackchart <- function(data){
-      # create echarts object
-      mychart <- data |> 
-        e_charts(day) |> 
-        e_bar(total, stack = "grp") |> 
-        e_bar(shortlisted, stack = "grp") |> 
-        e_bar(rejected, stack = "grp") |> 
-        e_bar(hold, stack = "grp")
+  # create echarts object
+  mychart <- data |> 
+    e_charts(day) |> 
+    e_bar(total, stack = "grp") |> 
+    e_bar(shortlisted, stack = "grp") |> 
+    e_bar(rejected, stack = "grp") |> 
+    e_bar(hold, stack = "grp") |>
+    e_tooltip(trigger = "axis", axisPointer = list(type = "shadow"))
     
-    return(mychart)
+  return(mychart)
 }
+
+
+
